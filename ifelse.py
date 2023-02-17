380843d795947673a478c342a4294198b89859dd
Nitin <Nitin@Nitins-mac.local>
2023-02-18 01:43:32 +0530

    first commit from 10

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number : "))
+b=float(input("enter the number : "))
+
+if(a>b) :
+    print("a is greater.")
+else:
+    print("b is greater.")
---
+++ sum.py
@@ -0,0 +1,5 @@
+a=5
+b=a
+print(a+b)
+a=10
+print(a+b)
