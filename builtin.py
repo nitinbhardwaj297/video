00b6984f6f976c0074a7e63f532f0b403a22d15a
Nitin <Nitin@Nitins-mac.local>
2023-02-23 13:41:53 +0530

    first commit

---
+++ builtin.py
@@ -0,0 +1,19 @@
+#abs
+# a = -20
+# b=4.83
+# print("the absolute value of these are :- ",abs(a))
+# print("the absolute value of these are :- ",abs(b))
+
+#bin - use to represent binary code
+# a=20
+# b=bin(a)
+# print(b)
+
+#sum
+# a=sum([1,2,3])
+# print(a)
+
+#float - it convert string to float
+# print(float(2.2))
+# print(float("12.332"))
+
